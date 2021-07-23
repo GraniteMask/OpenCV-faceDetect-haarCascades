7729a79a64696030ccdde62f255a0bd15e1e26ca
GraniteMask <ratnadeepastronomy@gmail.com>
2021-07-23 22:24:35 +0530

    face detect

--- face_detect.py
+++ face_detect.py
@@ -6,6 +6,18 @@ cv.imshow("Lady",img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray person', gray)
 
+haar_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+face_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+
+print(f'Number of faces found = {len(face_rect)}')
+
+for(x,y,w,h) in face_rect:
+    cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),thickness=2)
+
+cv.imshow('Detected faces',img)
+
+
 cv.waitKey(0)
 
-#haarcascade files are taken from opencv github rep --> data
+#haarcascade file are taken from opencv github rep --> data
